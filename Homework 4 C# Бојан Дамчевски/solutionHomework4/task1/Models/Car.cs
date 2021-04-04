2b6988ad09aa137c7d526a17bf6c525da35fe4a3
bojandamchevski <bojandamcevski@gmail.com>
2021-04-05 01:17:24 +0200

    homework 4

---
+++ Homework 4 C# Бојан Дамчевски/solutionHomework4/task1/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Driver Driver { get; set; }
+
+        public Car(string model, int speed)
+        {
+            Model = model;
+            Speed = speed;
+        }
+
+        public Car(string model, int speed, Driver driver)
+        {
+            Model = model;
+            Speed = speed;
+            Driver = driver;
+        }
+
+        public int CalculateSpeed(Driver driver)
+        {
+            int totalSpeed = 0;
+            totalSpeed = driver.Skill * Speed;
+            return totalSpeed;
+        }
+    }
+}
---
+++ Homework 4 C# Бојан Дамчевски/solutionHomework4/task1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using task1.Models;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("Choose car brands and drivers !");
+                Console.WriteLine("1.Bob 2.Greg 3.Jill 4.Anne");
+                Console.WriteLine("1.Hyundai 2.Mazda 3.Ferrari 4.Porsche");
+
+                Driver[] drivers =
+                {
+                new Driver("Bob", 5),
+                new Driver("Greg", 4),
+                new Driver("Jill", 3),
+                new Driver("Anne", 2)
+            };
+
+                Car[] cars =
+                {
+                new Car("Hyundai", 150),
+                new Car("Mazda", 200),
+                new Car("Ferrari", 250),
+                new Car("Porsche", 300)
+            };
+
+                Car[] raceCar1Object = new Car[1];
+                Car[] raceCar2Object = new Car[1];
+
+                int i = 0;
+                Console.WriteLine("Enter first driver");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter first car brand");
+                string car = Console.ReadLine();
+                int k = 0;
+
+                while (k < cars.Length)
+                {
+                    if (car.ToLower() == cars[k].Model.ToLower())
+                    {
+                        raceCar1Object[0] = cars[k];
+                        break;
+                    }
+                    k++;
+                }
+
+                while (i < drivers.Length)
+                {
+                    if (name.ToLower() == drivers[i].Name.ToLower())
+                    {
+                        raceCar1Object[0].Driver = drivers[i];
+                        break;
+                    }
+                    i++;
+                }
+                int j = 0;
+                Console.WriteLine("Enter second driver");
+                string name1 = Console.ReadLine();
+                Console.WriteLine("Enter second car brand");
+                string car1 = Console.ReadLine();
+
+                while (j < cars.Length)
+                {
+                    if (raceCar1Object[0].Model.ToLower() == car1.ToLower())
+                    {
+                        throw new Exception("Can not use the same car brand");
+                    }
+                    if (car1.ToLower() == cars[j].Model.ToLower())
+                    {
+                        raceCar2Object[0] = cars[j];
+                        break;
+                    }
+                    j++;
+                }
+
+                int e = 0;
+
+                while (e < drivers.Length)
+                {
+                    if (raceCar1Object[0].Driver.Name.ToLower() == name1.ToLower())
+                    {
+                        throw new Exception("Can not use the same racer");
+                    }
+                    if (name1.ToLower() == drivers[e].Name.ToLower())
+                    {
+                        raceCar2Object[0].Driver = drivers[e];
+                        break;
+                    }
+                    e++;
+                }
+
+                RaceCars(raceCar1Object[0], raceCar2Object[0]);
+
+                Console.WriteLine("Do you want to race again ? (yes/no)");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "no")
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+
+        }
+        static void RaceCars(Car car1, Car car2)
+        {
+            if (car1.CalculateSpeed(car1.Driver) > car2.CalculateSpeed(car2.Driver))
+            {
+                Console.WriteLine($"The racer {car1.Driver.Name} with the car {car1.Model} with speed of {car1.Speed}km/h is the winner !");
+            }
+            else if (car1.CalculateSpeed(car1.Driver) < car2.CalculateSpeed(car2.Driver))
+            {
+                Console.WriteLine($"The racer {car2.Driver.Name} with the car {car2.Model} with speed of {car2.Speed}km/h is the winner !");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie !");
+            }
+        }
+    }
+}
